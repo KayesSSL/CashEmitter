1db953e9a37856d38d0ff4356dba15ca896d1945
K.M. Imrul Kayes Sikdar <imrul.kayes@sslwireless.com>
2024-12-30 17:22:53 +0600

    changes path

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "CashEmitter",
     platforms: [
-        .iOS(.v12),
+        .iOS(.v14),
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
@@ -19,11 +19,11 @@ let package = Package(
             // Targets can depend on other targets in this package and products from dependencies.
             .binaryTarget(
                 name: "CashEmitter",
-                path: "./Sources/Debug/App.xcframework"
+                path: "./Sources/App.xcframework"
             ),
             .binaryTarget(
                 name: "Flutter",
-                path: "./Sources/Debug/Flutter.xcframework"
+                path: "./Sources/Flutter.xcframework"
             ),
         ]
 )
